5418f7986813ee6d69645f8d147810a9bd1388bc
Paul Hutchinson <phutchinson@tantalus.com>
2020-07-24 09:53:14 -0400

    Fixed up the copyright at the top of the files.

--- PreCompiler/src/TimeStamp.h
+++ PreCompiler/src/TimeStamp.h
@@ -2,10 +2,28 @@
  * FILENAME: TimeStamp.h
  * 
  * PROJECT:
- *    Bitty HTTP
+ *    WebC
  *
  * FILE DESCRIPTION:
  *    
+ *
+ * COPYRIGHT:
+ *    Copyright (C) 2019  Paul Hutchinson
+ *
+ *    This file is part of WebC.
+ *
+ *    This WebC is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * HISTORY:
  *    Paul Hutchinson (23 Oct 2019)
--- PreCompiler/src/main.c
+++ PreCompiler/src/main.c
@@ -19,12 +19,57 @@ Block Rules:
     * <? ?> at top is global space
     * any HTML goes into page function unless the C scope is > 0
 */
+
+/*******************************************************************************
+ * FILENAME: main.c
+ *
+ * PROJECT:
+ *    WebC
+ *
+ * FILE DESCRIPTION:
+ *    
+ *
+ * COPYRIGHT:
+ *    Copyright (C) 2020  Paul Hutchinson
+ *
+ *    This file is part of WebC.
+ *
+ *    This WebC is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * CREATED BY:
+ *    Paul Hutchinson (24 Jul 2020)
+ *
+ ******************************************************************************/
+
+/*** HEADER FILES TO INCLUDE  ***/
 #include <stdio.h>
 #include "CmdLine.h"
 #include "TimeStamp.h"
 #include "Options.h"
 #include "SourceFiles.h"
 #include "ProcessFile.h"
+
+/*** DEFINES                  ***/
+
+/*** MACROS                   ***/
+
+/*** TYPE DEFINITIONS         ***/
+
+/*** FUNCTION PROTOTYPES      ***/
+
+/*** VARIABLE DEFINITIONS     ***/
 
 int main(int argv,char **argc)
 {
